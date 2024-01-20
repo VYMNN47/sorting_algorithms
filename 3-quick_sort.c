a12482914a00e1e1b896edde0743d9ef190e37f2
Ahmed <ahmed_l2@yahoo.com>
2024-01-20 22:24:58 +0300

    3. Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,51 @@
+#include "sort.h"
+
+void swap(int *array, size_t size, int *a, int *b)
+{
+        int temp;
+
+	if (*a != *b)
+	{
+		temp = *a;
+	        *a = *b;
+        	*b = temp;
+		print_array(array, size);
+	}
+}
+
+size_t lomuto_part(int *array, size_t size, ssize_t min, ssize_t max)
+{
+	int i, j, piv = array[max];
+
+	for (i = j = min; j < max; j++)
+	{
+		if (array[j] < piv)
+		{
+			swap(array, size, &array[j], &array[i++]);
+		}
+	}
+	swap(array, size, &array[i], &array[max]);
+
+	return (i);
+}
+
+void quicksort_helper(int *array, size_t size, ssize_t min, ssize_t max)
+{
+	if (min < max)
+	{
+		size_t piv = lomuto_part(array, size, min, max);
+
+		quicksort_helper(array, size, min, piv - 1);
+		quicksort_helper(array, size, piv + 1, max);
+	}
+}
+
+void quick_sort(int *array, size_t size)
+{
+	if (!array || !size)
+	{
+		return;
+	}
+
+	quicksort_helper(array, size, 0, size - 1);
+}
