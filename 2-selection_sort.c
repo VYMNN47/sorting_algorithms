bf27b3808bd53704d58a8a03ed84f031760baa78
aymnn <ayman.belahsen.4@gmail.com>
2024-01-20 20:39:48 +0100

    2.Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,33 +3,33 @@
 
 void swap(int *a, int *b)
 {
-        int temp = *a;
-        *a = *b;
-        *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 void selection_sort(int *array, size_t size)
 {
-        size_t i, j, min;
+	size_t i, j, min;
 
 	if (!array || !size)
 	{
 		return;
 	}
 
-        for (i = 0; i < size - 1; i++)
-        {
-                for (j = size - 1, min = i + 1; j > i; j--)
-                {
-                        if (array[j] < array[min])
-                        {
-                                min = j;
-                        }
-                }
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = size - 1, min = i + 1; j > i; j--)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
 		if (array[i] > array[min])
 		{
-                	swap(&array[min], &array[i]);
-	                print_array(array, size);
+			swap(&array[min], &array[i]);
+			print_array(array, size);
 		}
-        }
+	}
 }
