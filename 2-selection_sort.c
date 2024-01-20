fce5ff666507bd5f363bb759a920d2e180dd77c2
Ahmed <ahmed_l2@yahoo.com>
2024-01-20 19:11:01 +0300

    2. Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+
+void swap(int *a, int *b)
+{
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+}
+
+void selection_sort(int *array, size_t size)
+{
+        size_t i, j, min;
+
+	if (!array || !size)
+	{
+		return;
+	}
+
+        for (i = 0; i < size - 1; i++)
+        {
+                min = i;
+
+                for (j = i+1; j < size; j++)
+                {
+                        if (array[j] < array[min])
+                        {
+                                min = j;
+                        }
+                }
+
+                swap(&array[min], &array[i]);
+                print_array(array, size);
+        }
+}
